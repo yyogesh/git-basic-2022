34b9ea8632cbd64941a7037c02322d10fd8c62b0
yyogesh <yogeshggn1@gmail.com>
2022-08-03 14:34:20 +0530

    addition of two number

--- src/app.component.js
+++ src/app.component.js
@@ -1,2 +1,6 @@
 // this is app component 
-console.log("its root compoonent ==> app.componen.js")
+console.log("its root compoonent ==> app.componen.js");
+
+var a = 10;
+var b = 20;
+var c = a + b;
